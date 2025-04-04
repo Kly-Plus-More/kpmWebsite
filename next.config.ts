b8627283b11546ebf274acbb0163c1a6c90dfe05
azizben01 <benazizsang1@gmail.com>
2025-04-04 06:00:37 +0200

    production bugs

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // Other config options...
-  server: {
-    port: 3001, // Custom port
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
