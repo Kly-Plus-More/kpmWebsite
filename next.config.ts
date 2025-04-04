efebc42ecc2fb54d73c067d23517cca05310e5d3
azizben01 <benazizsang1@gmail.com>
2025-04-04 05:53:37 +0200

    production bug solved 3001

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Other config options...
+  server: {
+    port: 3001, // Custom port
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
